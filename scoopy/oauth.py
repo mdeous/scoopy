211e32ed3b2b4b4987f8abe88e39e3a4adc52e0c
MatToufoutu <mattoufootu@gmail.com>
2011-05-07 21:59:17 +0200

    improve oauth module

--- scoopy/oauth.py
+++ scoopy/oauth.py
@@ -17,6 +17,7 @@
 #
 
 from time import time
+from urllib import urlencode
 from urlparse import parse_qsl
 
 import oauth2
@@ -28,6 +29,7 @@ ACCESS_TOKEN_URL = '%s/oauth/access' % BASE_URL
 AUTHORIZE_URL = '%s/oauth/authorize' % BASE_URL
 
 
+#noinspection PyMissingConstructor
 class OAuthException(Exception):
     """
     Basic exception for OAuth related errors.
@@ -38,6 +40,21 @@ class OAuthException(Exception):
         return repr(self.value)
 
 
+class OAuthRequestFailure(OAuthException):
+    """
+    Exception raised when a request fails.
+    """
+    pass
+
+
+class OAuthTokenError(OAuthException):
+    """
+    Exception raised when a token isn't set and
+    an operation requiring one is performed.
+    """
+    pass
+
+
 class OAuth(object):
     """
     Helper class for all OAuth related actions.
@@ -46,13 +63,13 @@ class OAuth(object):
         - consumer_key (str): the application's API consumer key
         - consumer_secret (str): the application's API consumer secret
     """
+    signature_method = oauth2.SignatureMethod_HMAC_SHA1()
+
     def __init__(self, consumer_key, consumer_secret):
         self.consumer = oauth2.Consumer(consumer_key, consumer_secret)
         self.client = oauth2.Client(self.consumer)
-        self.request_token = None
-        self.request_token_secret = None
-        self.access_token = None
-        self.access_token_secret = None
+        self.token = None
+        self.access_granted = False
 
     def get_request_token(self):
         """
@@ -60,29 +77,70 @@ class OAuth(object):
         """
         response, content = self.client.request(REQUEST_TOKEN_URL, 'GET')
         if response['status'] != '200':
-            raise OAuthException("Failed to get request token (%s)" % response['status'])
+            raise OAuthRequestFailure(
+                "Failed to get request_token (%s)" % response['status']
+            )
         request_token = dict(parse_qsl(content))
-        self.request_token = request_token['oauth_token']
-        self.request_token_secret = request_token['oauth_token_secret']
-        return self.request_token, self.request_token_secret
+        self.token = oauth2.Token(
+            request_token['oauth_token'],
+            request_token['oauth_token_secret']
+        )
 
-    def get_access_token_url(self):
+    def get_access_token_url(self, callback_url):
         """
-        Generate the URL needed for the user to accept the application and return it.
+        Generate the URL needed for the user to accept the application
+        and return it.
         """
-        if self.request_token is None:
-            raise OAuthException("Request token is not set")
-        return "%s?oauth_token=%s" % (AUTHORIZE_URL, self.request_token)
+        if self.token is None:
+            raise OAuthTokenError(
+                "No request_token found, get one first"
+            )
+        #TODO: warn user if access already granted
+        return "%s?oauth_token=%s&oauth_callback=%s" % (
+            AUTHORIZE_URL,
+            self.token.key,
+            callback_url
+        )
 
     def get_access_token(self, token_verifier):
         """
         Request the server for an access token and return it.
         """
-        token = oauth2.Token(self.request_token, self.request_token_secret)
-        token.set_verifier(token_verifier)
-        self.client = oauth2.Client(self.consumer, token)
+        self.token.set_verifier(token_verifier)
+        self.client = oauth2.Client(self.consumer, self.token)
         response, content = self.client.request(ACCESS_TOKEN_URL, 'POST')
+        if response['status'] != '200':
+            raise OAuthRequestFailure(
+                "Failed to get access_token (%s)" % response['status']
+            )
+        self.access_granted = True
         access_token = dict(parse_qsl(content))
-        self.access_token = access_token['oauth_token']
-        self.access_token_secret = access_token['oauth_token_secret']
-        return self.access_token, self.access_token_secret
+        self.token = oauth2.Token(
+            access_token['oauth_token'],
+            access_token['oauth_token_secret'],
+        )
+        self.client = oauth2.Client(self.consumer, self.token)
+
+    def generate_request_params(self, params):
+        """
+        Given a dict of parameters, add the needed oauth_* parameters
+        to it and an url-encoded string.
+        """
+        request_params = {
+            'oauth_version':        '1.0',
+            'oauth_nonce':          oauth2.generate_nonce(),
+            'oauth_timestamp':      int(time()),
+            'oauth_token':          self.token.key,
+            'oauth_consumer_key':   self.consumer.key,
+        }
+        for key, value in params.iteritems():
+            request_params[key] = value
+        return urlencode(request_params)
+
+    def request(self, url, params, method='GET'):
+        return self.client.request(
+            url,
+            method=method,
+            body=self.generate_request_params(params),
+            headers={'Accept-encoding': 'gzip'},
+        )
