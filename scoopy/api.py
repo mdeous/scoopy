d4a4052ebb1ff9881c6e67818b73a844b7b90284
MatToufoutu <mattoufootu@gmail.com>
2011-12-02 11:11:01 +0100

    add a sphinx docstring at top of scoopy/api.py

--- scoopy/api.py
+++ scoopy/api.py
@@ -15,6 +15,11 @@
 #    You should have received a copy of the GNU General Public License
 #    along with Scoopy.  If not, see <http://www.gnu.org/licenses/>.
 #
+"""
+.. module:: api
+
+.. moduleauthor:: Mathieu D. (MatToufoutu) <mattoufootu[at]gmail.com>
+"""
 
 try:
     import json # python >= 2.6
